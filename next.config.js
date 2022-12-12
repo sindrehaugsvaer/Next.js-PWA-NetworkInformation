c2004b7c418e1c91079b24f5f106c8023880238b
sindrehaugsvaer <sindre.haugsvaer@gmail.com>
2022-12-12 20:14:45 +0100

    Mod

--- next.config.js
+++ next.config.js
@@ -1,7 +1,43 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// // const nextConfig = {
+//   //   reactStrictMode: true,
+//   //   swcMinify: true,
+//   // }
+  
+//   // module.exports = nextConfig
+  
+// /** @type {import('next').NextConfig} */
+// const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+// // module.exports = withPWA({
+// //   pwa: {
+// //     dest: "public",
+// //     register: true,
+// //     skipWaiting: true,
+// //   },
+// // });
+
+// const nextConfig = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+//   reactStrictMode: true,
+// });
+
+// module.exports = nextConfig;
+
+// NEW:
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
+
+module.exports = withPWA({
+  // next.js config
+})
