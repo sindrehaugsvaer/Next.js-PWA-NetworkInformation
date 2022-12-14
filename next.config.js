b6068f7afd9e8e8330b271822dba8fcb69e5abf5
Sindre Haugsvær <sindre.haugsvaer@telenor.no>
2022-12-14 22:07:20 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-const withPWA = require('next-pwa');
-({
-  dest: 'public'
-  // disable: process.env.NODE_ENV === 'development',
-  // register: true,
-  // scope: '/app',
-  // sw: 'service-worker.js',
-  //...
-})
+// const withPWA = require('next-pwa');
+// ({
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development',
+//   register: true,
+//   scope: '/app', //!!! jeg har ikke en /app-mappe
+//   sw: 'service-worker.js', //!!! min sw heter bare sw
+//   ...
+// })
 
-module.exports = withPWA({
-  images: {
-    domains: ['images.unsplash.com'],
-    // more domains? 'https://cdn.pixabay.com', 'https://i.ntnu.no', 
-  }
-})
+// module.exports = withPWA({
+//   images: {
+//     domains: ['images.unsplash.com'],
+//     // more domains? 'https://cdn.pixabay.com', 'https://i.ntnu.no', 
+//   }
+// })
 
 // module.exports = withPWA({
 //   pwa: {
@@ -22,3 +22,19 @@ module.exports = withPWA({
 //     skipWaiting: true,
 //   },
 // });
+
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  },
+  images: {
+    domains: ['images.unsplash.com']
+    // more domains? 'https://cdn.pixabay.com', 'https://i.ntnu.no', 
+  }
+});
