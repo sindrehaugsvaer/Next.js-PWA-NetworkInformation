c3fda9ef9c86420e434c7358f6b844bfc9250708
Sindre Haugsvær <sindre.haugsvaer@telenor.no>
2022-12-14 22:02:27 +0100

    config file change

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const withPWA = require('next-pwa');
-// ({
-//   dest: 'public'
-//   // disable: process.env.NODE_ENV === 'development',
-//   // register: true,
-//   // scope: '/app',
-//   // sw: 'service-worker.js',
-//   //...
-// })
+({
+  dest: 'public'
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
 
 module.exports = withPWA({
   images: {
