6c5e2c4bcbf1a211029df9f9b5608e70b3059aa6
Sindre Haugsvær <sindre.haugsvaer@telenor.no>
2022-12-14 21:59:30 +0100

    Responsive + design changes.

--- next.config.js
+++ next.config.js
@@ -1,63 +1,18 @@
-// // const nextConfig = {
-//   //   reactStrictMode: true,
-//   //   swcMinify: true,
-//   // }
-
-//   // module.exports = nextConfig
-
-// /** @type {import('next').NextConfig} */
-// const withPWA = require("next-pwa");
-
-// // module.exports = withPWA({
-// //   pwa: {
-// //     dest: "public",
-// //     register: true,
-// //     skipWaiting: true,
-// //   },
-// // });
-
-// const nextConfig = withPWA({
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//   },
-//   reactStrictMode: true,
-// });
-
-// module.exports = nextConfig;
-
-// NEW:
-
-const withPWA = require('next-pwa')({
-  dest: 'public'
-  // disable: process.env.NODE_ENV === 'development',
-  // register: true,
-  // scope: '/app',
-  // sw: 'service-worker.js',
-  //...
-})
+const withPWA = require('next-pwa');
+// ({
+//   dest: 'public'
+//   // disable: process.env.NODE_ENV === 'development',
+//   // register: true,
+//   // scope: '/app',
+//   // sw: 'service-worker.js',
+//   //...
+// })
 
 module.exports = withPWA({
   images: {
     domains: ['images.unsplash.com'],
+    // more domains? 'https://cdn.pixabay.com', 'https://i.ntnu.no', 
   }
-  // images: {
-  //   domains: ['https://images.unsplash.com'],
-  // },
-  // images: {
-  //   formats: ['image/avif', 'image/webp'],
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'images.unsplash.com',
-  //       port: '',
-  //       pathname: '/image/upload/**',
-  //     },
-  //   ],
-  // },
-  // next.js config
-  // more img: 'https://cdn.pixabay.com', 'https://i.ntnu.no', 
 })
 
 // module.exports = withPWA({
@@ -67,14 +22,3 @@ module.exports = withPWA({
 //     skipWaiting: true,
 //   },
 // });
-
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'example.com',
-  //       port: '',
-  //       pathname: '/account123/**',
-  //     },
-  //   ],
-  // },
--- pages/index.tsx
+++ pages/index.tsx
@@ -53,45 +53,82 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.home}>
-          <h1 className={styles.title}>
-            <a href="https://www.ntnu.edu/studies/courses/IMT4894/">APW</a> project<br></br>Fall 2022
-          </h1>
+          <div className={styles.titleContentWidth}>
+            <h1 className={styles.title}>
+              <a href="https://www.ntnu.edu/studies/courses/IMT4894/">APW</a> project<br></br>Fall 2022
+            </h1>
 
-          <p className={styles.description}>
-            This progressive web app renders content dynamically <br></br> based on your network connection by using the <br></br>
-            <a href="https://developer.mozilla.org/en-US/docs/Web/API/Network_Information_API">network information API</a>
-            {/* <code className={styles.code}>network information API</code> */}
-          </p>
+            <p className={styles.description}>
+              This progressive web app renders content dynamically based on your network connection by using the
+              <a href="https://developer.mozilla.org/en-US/docs/Web/API/Network_Information_API"> network information API</a>
+            </p>
 
-          <p className={styles.instructions}>
-            Please use the &quot;throttling&quot; option available in<br></br> the Network-tab in the developer-tools to<br></br> adjust the content displayed on this site.
-          </p>
+            <div className={styles.speedBox}>
+              <p>Current connection equivalent to:</p>
+              <h2>{(connection.effectiveType == "offline") ? 'off' : connection.effectiveType}</h2>
+            </div>
+
+            <p className={styles.instructions}>
+              Please use the &quot;throttling&quot; option available in the Network-tab in the developer-tools to adjust the content displayed on this site.
+            </p>
+          </div>
 
           <div className={styles.statusSpace}>
-            <h2>Your current stats:</h2>
-            <div className={styles.statusGrid}>
-              <div className={styles.statusBox}>
-                <p>Speed: <a className={styles.stats}>{connection.effectiveType}</a></p>
-              </div>
-              {/* <h3 className={styles.centerText}>Type: {connection.type}</h3> */}
-              <div className={styles.statusBox}>
-                <p>Downlink: <a className={styles.stats}>{connection.downlink}</a></p>
-              </div>
-              <div className={styles.statusBox}>
-                <p>RTT: <a className={styles.stats}>{connection.rtt}</a></p>
-              </div>
-              {/* console.log(`This device has at least ${memory}GiB of RAM.`) */}
-              <div className={styles.statusBox}>
-                <p>RAM: <a className={styles.stats}>{ram}GB</a></p>
-              </div>
-              <div className={styles.statusBox}>
-                <p>Battery charging: <a className={styles.stats}>{battery.charging}</a></p>
-              </div>
-              <div className={styles.statusBox}>
-                <p>Battery level: <a className={styles.stats}>{battery.level}</a></p>
+            <p className={styles.scrollDownInfo}>&darr; Scroll down to view the <a className={styles.stats}>{version}</a> version of the app &darr;</p>
+            <div className={styles.statusBigBox}>
+              <h3>Your current stats (click to read more):</h3>
+              <div className={styles.statusGrid}>
+
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation/effectiveType"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.statusBox}>
+                  <p>Speed: </p>
+                  <p className={styles.stats}>{connection.effectiveType}</p>
+                </a>
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation/downlink"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.statusBox}>
+                  <p>Downlink: </p>
+                  <p className={styles.stats}>{connection.downlink}</p>
+                </a>
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation/rtt"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.statusBox}>
+                  <p>RTT:</p>
+                  <p className={styles.stats}>{connection.rtt}</p>
+                </a>
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/Web/API/Navigator/deviceMemory"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.statusBox}>
+                  <p>RAM:</p>
+                  <p className={styles.stats}>{ram}GB</p>
+                </a>
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/Web/API/Navigator/getBattery"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.statusBox}>
+                  <p>Battery charging:</p>
+                  <p className={styles.stats}>{battery.charging}</p>
+                </a>
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/Web/API/Navigator/getBattery"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.statusBox}>
+                  <p>Battery level:</p>
+                  <p className={styles.stats}>{battery.level}</p>
+                </a>
               </div>
             </div>
-            <p>&darr; Currently displaying the <a className={styles.stats}>{version}</a> version of the app &darr;</p>
           </div>
         </div>
 
@@ -113,6 +150,9 @@ export default function Home() {
                   alt="Space"
                   src="https://images.unsplash.com/photo-1502134249126-9f3755a50d78?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
                   fill
+                  sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw"
                 />
               </div>
             </a>
@@ -126,12 +166,15 @@ export default function Home() {
                   alt="Space"
                   src="https://images.unsplash.com/photo-1596827414894-911f1265e391?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
                   fill
+                  sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw"
                 />
               </div>
             </a>
 
             <a href="https://www.nasa.gov/" target="_blank" rel="noopener noreferrer" className={styles.card}>
-              <h2>Lorem &rarr;</h2>
+              <h2>Black hole &rarr;</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               <div className={styles.imgContainer}>
                 <Image
@@ -139,6 +182,9 @@ export default function Home() {
                   alt="Space"
                   src="https://images.unsplash.com/photo-1640984756059-7303641db7cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
                   fill
+                  sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw"
                 />
               </div>
             </a>
@@ -146,9 +192,12 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <p>Created by Sindre</p>
-      </footer>
+
+      <a href="https://github.com/sindrehaugsvaer/Next.js-PWA-NetworkInformation" target="_blank" rel="noopener noreferrer">
+        <footer className={styles.footer}>
+          <p>Created by Sindre</p>
+        </footer>
+      </a>
     </div>
   )
 }
